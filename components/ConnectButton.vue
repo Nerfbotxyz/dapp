9041b26c459bb520c0279a8e07456f8258b2e7bb
Jim Toth <jim@artby.city>
2023-09-29 04:21:34 -0400

    Checks api key for existence / validity on connect

--- components/ConnectButton.vue
+++ components/ConnectButton.vue
@@ -1,6 +1,9 @@
 <template>
   <v-menu v-if="auth.isLoggedIn">
     <template #activator="{ props }">
+      <code v-if="auth.label" style="cursor: pointer;" v-bind="props">
+        <strong>{{ auth.label }}</strong> |
+      </code>
       <code style="cursor: pointer;" v-bind="props">{{ auth.apiKey }}</code>
     </template>
     <v-list>
@@ -19,9 +22,10 @@
     Connect
   </v-btn>
 
-  <v-dialog v-model="isDialogOpen">
+  <v-dialog v-model="isDialogOpen" persistent>
     <template #default>
-      <v-card title="Connect">
+      <v-card>
+        <v-card-title>Connect</v-card-title>
         <v-form v-model="isFormValid" @submit.prevent="onDialogConfirmed">
           <v-card-text>
             <v-text-field v-model="apiKey" label="API Key" :rules="rules" />
@@ -39,6 +43,20 @@
       </v-card>
     </template>
   </v-dialog>
+
+  <v-snackbar v-model="isErrorSnackbarOpen" color="error">
+    API Key not found!
+
+    <template #actions>
+      <v-btn
+        color="black"
+        variant="text"
+        @click="isErrorSnackbarOpen = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script setup lang="ts">
@@ -46,18 +64,23 @@ import { useAuthStore } from '~/stores/auth'
 const apiKey = ref('')
 const isFormValid = ref(false)
 const isDialogOpen = ref(false)
+const isErrorSnackbarOpen = ref(false)
 const rules = [ (value: string) => !!value || 'Required' ]
 const auth = useAuthStore()
 const onConnectClicked = debounce(() => isDialogOpen.value = true)
 const onDialogConfirmed = debounce(async () => {
   try {
     if (!isFormValid.value) { return }
-    await auth.connect(apiKey.value)
-    isDialogOpen.value = false
+    const success = await auth.connect(apiKey.value)
+    if (success) {
+      isDialogOpen.value = false
+    } else {
+      isErrorSnackbarOpen.value = true
+    }
   } catch (error) {
     console.error(error)
   }
 })
 const onDialogCancelled = debounce(() => isDialogOpen.value = false)
-const onDisconnectClicked = debounce(async () => await auth.disconnect())
+const onDisconnectClicked = debounce(() => auth.disconnect())
 </script>
--- stores/auth.ts
+++ stores/auth.ts
@@ -3,17 +3,27 @@ import { defineStore } from 'pinia'
 interface AuthState {
   _isBootstrapped: boolean
   apiKey: string | null
-  label: string | null
+  label?: string
+}
+
+interface ApiKey {
+  apiKey: string
+  apiKeyId: number
+  created_at: string
+  update_at: string
+  label?: string
+  userId: number
 }
 
 export const useAuthStore = defineStore('auth', {
   state: (): AuthState => {
-    return { _isBootstrapped: false, apiKey: null, label: null }
+    return { _isBootstrapped: false, apiKey: null }
   },
   getters: {
     isLoggedIn: ({ apiKey }) => !!apiKey
   },
   actions: {
+    /* eslint-disable-next-line @typescript-eslint/require-await */
     async bootstrap() {
       if (!this._isBootstrapped) {
         // TODO -> get apiKey from localstorage :)
@@ -22,12 +32,36 @@ export const useAuthStore = defineStore('auth', {
       }
     },
     async connect(apiKey: string) {
-      // TODO -> test that this is a valid api key by trying to use it probably
-      this.apiKey = apiKey
+      const { public: { apiBaseUrl } } = useRuntimeConfig()
+      let success = false
+      try {
+        const response = await fetch(
+          `${apiBaseUrl}/auth/api-keys?token=${apiKey}`
+        )
+        success = response.ok
+
+        const { apiKeys } = await response.json() as { apiKeys: ApiKey[] }
+        console.log('apiKeys', apiKeys)
+        const thisApiKey = apiKeys.find(a => a.apiKey === apiKey)
+        if (thisApiKey) {
+          this.label = thisApiKey.label
+          this.apiKey = thisApiKey.apiKey
+        } else {
+          success = false
+        }
+        
+        if (success) {
+          this.apiKey = apiKey
+        }
+      } catch (error) {
+        console.error('Error on auth connect', error)
+      }
+
+      return success
     },
     disconnect() {
       this.apiKey = null
-      this.label = null
+      this.label = undefined
     }
   }
 })
